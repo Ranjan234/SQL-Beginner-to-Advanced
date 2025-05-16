e2b65a06c4e58b1a7d0ce702b5c3286df35645e5
Soumya Ranjan Sahoo <soumyaranjansahoo840@gmail.com>
2025-05-16 20:37:29 +0530

    Add files via upload

---
+++ 01_Basics.sql
@@ -0,0 +1,26 @@
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT * 
+FROM parks_and_recreation.employee_salary;
+
+SELECT * 
+FROM parks_and_recreation.parks_departments;
+
+
+
+SELECT first_name,
+ last_name, birth_date
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name,
+ last_name, birth_date,
+ age,
+ (age + 10) * 10 + 10 
+FROM parks_and_recreation.employee_demographics;
+
+# PEMDAS -- order operation 
+-- Parenthesis, Exponent, Multiplication, Division, Addition, Subtract
+
+SELECT  distinct  first_name, gender
+FROM parks_and_recreation.employee_demographics;
